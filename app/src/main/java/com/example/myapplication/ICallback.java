e30feb4fb18fe3e4b9f7811578c56d59a2626525
hejing <hejing@hoodinn.com>
2021-12-01 11:23:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ICallback.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+public interface ICallback {
+
+
+    void onFilure(Exception result);
+    void onSuccess(Object result);
+    /**
+     * 将从服务器得到的HttpResponse进行解析，解析完成以后，返回给UI线程
+     */
+//    Object handle(HttpResponse response);
+
+
+}
